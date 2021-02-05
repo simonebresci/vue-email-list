b49f7ec2942db30cab28c9bef7709e9b264df831
Simone <simone.bresci.05@gmail.com>
2021-02-05 16:20:22 +0100

    ricezione 10 mail con axios

--- js/main.js
+++ js/main.js
@@ -1,36 +1,35 @@
 /* Eseguire in Vue.js una chiamata ajax, generare 10 email e stamparle a schermo.
 La chiamata ajax vi ritornerà ogni volta un'email random.
 Endpoint: https://flynn.boolean.careers/exercises/api/random/mail
-Buon lavoro, branco! */
+*/
 
 
 
-
+// VUE *************************************************************************
 var app = new Vue ({
   el: '#root',
   data: {
-    listaTodo: ['Fare i compiti', 'Fare la spesa', 'Fare il bucato'],
-    inputAttivita: ''
+    listaEmail: []
   },
   mounted() {
-    // alert('Eseguo axios nel mounted');
+    const self = this;
+    for(let i = 0; i < 10; i ++){
+      // Prendi mail generata dal link
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(objReceived){
+             const result = objReceived.data.response;
+             self.pushElement(result);
+           });
 
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(objReceived){
-           // alert('sono nel then');
-           const result = objReceived.data.response;
-           console.log(result);
-         });
+    }
 
   },
-  methods : {
-    pushElement: function (){
-      if(this.inputAttivita != ''){
-        this.listaTodo.push(this.inputAttivita);
-      }
+  methods: {
+    pushElement: function (elemento){
+      this.listaEmail.push(elemento);
     },
     removeElement: function (index){
       // Rimuovere elemento con funzione .filter
-      this.listaTodo = this.listaTodo.filter( (element,i) =>{
+      this.listaEmail = this.listaEmail.filter( (element,i) =>{
         return (i != index);
       });
     }
@@ -38,3 +37,5 @@ var app = new Vue ({
 
 });
 Vue.config.devtools = true;
+
+// / VUE ***********************************************************************
